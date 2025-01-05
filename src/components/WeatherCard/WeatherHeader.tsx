112bea0a0cfc990b97c73b1b28188baf6ad168c3
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-05 00:30:52 -0300

    feat(API): Consume ipinfo API for geolocation

--- src/components/WeatherCard/WeatherHeader.tsx
+++ src/components/WeatherCard/WeatherHeader.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FaLocationDot } from "react-icons/fa6";
+const key_ipinfo=import.meta.env.VITE_AK_IPINFO
 export const WeatherHeader:React.FC = () => {
+  const [location, setLocation]=useState('')
+  const [city, setCity]=useState("Bernal")
+  const [region, setRegion]=useState("Buenos Aires")
+  const [country, setCountry]=useState("AR")
+  
+  useEffect(()=>{
+    const fetchLocationData= async ()=>{
+      const rawData=await fetch(`https://ipinfo.io/json?token=${key_ipinfo}`)
+      const data=await rawData.json()
+      setCity(data.city)
+      setRegion(data.region)
+      setCountry(data.country)
+      setLocation(data.loc)
+    }
+    fetchLocationData()
+  },[])
+  console.log(location)
   return (
     <section className='weatherHeader'>
         <div className='weatherHeader__time'>
@@ -9,7 +27,7 @@ export const WeatherHeader:React.FC = () => {
         </div>
         <div className='weatherHeader__location'>
             <FaLocationDot/>
-            <p>Ezpeleta, Buenos Aires, Argentina</p>
+            <p>{`${city}, ${region}, ${country}`}</p>
         </div>
     </section>
   )
