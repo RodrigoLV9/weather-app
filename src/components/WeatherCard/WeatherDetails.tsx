62488494fbe8ad3e50d46ed919304c108e8f6687
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-01-14 22:29:19 -0300

    feat(WeatherCard): Synchronize with ContextSettings and add functions for calculate the different values

--- src/components/WeatherCard/WeatherDetails.tsx
+++ src/components/WeatherCard/WeatherDetails.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { PiSpeedometerFill } from "react-icons/pi";
 import { WiHumidity } from "react-icons/wi";
 import { FaWind } from "react-icons/fa";
 import { WiWindDeg } from "react-icons/wi";
 import { useLanguage } from '../Context/ContextLanguage';
+import { useSettings } from '../Context/ContextSettings';
 interface DetailsValues{
     humidity?:number,
     pressure?:number,
@@ -12,6 +13,20 @@ interface DetailsValues{
 }
 export const WeatherDetails:React.FC<DetailsValues> = ({humidity=0,pressure=0,wind_speed=0,wind_deg=0}) => {
     const {language}=useLanguage()
+    const {settings}=useSettings()
+    const [wind, setWind]=useState<number | undefined>(undefined)
+    const calculateWindSpeed=(windSpeedValue:number,unit:'km/h'|'m/s')=>{
+        const calculateWind={
+            'km/h':windSpeedValue,
+            'm/s':windSpeedValue/3.6
+        }
+        return calculateWind[unit]
+    }
+    useEffect(()=>{
+        const windSpeedResult=calculateWindSpeed(wind_speed, settings?.wind as 'km/h' | 'm/s')
+        setWind(windSpeedResult)
+    },[settings?.wind, wind_speed])
+
   return (
     <section className="weatherDetails">
         <div className="weatherDetails__item">
@@ -24,11 +39,11 @@ export const WeatherDetails:React.FC<DetailsValues> = ({humidity=0,pressure=0,wi
         </div>
         <div className="weatherDetails__item">
             <FaWind className='weatherDetails__item-icon'/>
-            <p>{language ? 'Wind:':'Viento'} {wind_speed} km/h</p>
+            <p>{language ? 'Wind:':'Viento'} {wind?.toFixed(2)} {settings?.wind}</p>
         </div>
         <div className="weatherDetails__item">
             <WiWindDeg className='weatherDetails__item-icon'/>
-            <p>{language ? 'Wind angle' : 'Angulo viento:'} {wind_deg}deg</p>
+            <p>{language ? 'Wind angle:' : 'Angulo viento:'} {wind_deg}°</p>
         </div>
     </section>
   )
--- src/components/WeatherCard/WeatherTemperature.tsx
+++ src/components/WeatherCard/WeatherTemperature.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { useLanguage } from '../Context/ContextLanguage'
+import { useSettings } from '../Context/ContextSettings'
 interface WeatherValues{
   description?:string,
   icon?:string,
@@ -8,20 +9,54 @@ interface WeatherValues{
   temp_max?:number,
   temp_min?:number
 }
+interface Temperature{
+  temp:number,
+  temp_sensation:number,
+  temp_min:number,
+  temp_max:number
+
+}
 export const WeatherTemperature:React.FC<WeatherValues>= ({description='', icon='13d', temp=0, thermal_sensation=0, temp_max=0, temp_min=0}) => {
+  const [temperature, setTemperature]=useState<Temperature | undefined>(undefined)
+  const {settings}=useSettings()
   const {language}=useLanguage()
+  const calculateTemp=(temp:number, unit: '°F' | '°K' | '°C')=>{
+    const calculate={
+      '°F':(temp*9)/5 + 32,
+      '°K':temp+273.15,
+      '°C':temp
+    }
+    return calculate[unit]
+  }
+  const handleTemperatureValue=(temp:number, temp_sensation:number, temp_min:number, temp_max:number, unit:'°F' | '°K' | '°C')=>{
+    const temp1=calculateTemp(temp, unit)
+    const sensation=calculateTemp(temp_sensation,unit)
+    const min=calculateTemp(temp_min, unit)
+    const max=calculateTemp(temp_max,unit)
+    setTemperature({
+      temp:temp1,
+      temp_sensation:sensation,
+      temp_min:min,
+      temp_max:max
+    })
+  }
+  useEffect(() => {
+    if (settings?.temperature) {
+      handleTemperatureValue(temp,thermal_sensation, temp_min, temp_max, settings.temperature as '°F' | '°K' | '°C');
+    }
+  }, [settings?.temperature, temp]);
   return (
     <section className='weatherTemperature'>
         <p>{description.charAt(0).toUpperCase() + description.slice(1)}</p>
         <div className="temperature">
             <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="weather-icon" className='temperature__icon' />
-            <p>{temp}°C</p>
+            <p>{temperature?.temp}{settings?.temperature}</p>
         </div>
-        <p>{language ? 'Thermal sensation:' : 'Sensacion Termica'} {thermal_sensation}°C</p>
+        <p>{language ? 'Thermal sensation:' : 'Sensacion Termica'} {temperature?.temp_sensation.toFixed(2)}{settings?.temperature}</p>
         <div className='temp-minmax'>
-            <p className='temp-min'>{temp_min}°C </p>
+            <p className='temp-min'>{temperature?.temp_min.toFixed(2)}{settings?.temperature}</p>
             <p>/</p>
-            <p className='temp-max'>{temp_max}°C</p>
+            <p className='temp-max'>{temperature?.temp_max.toFixed(2)}{settings?.temperature}</p>
         </div>
     </section>
   )
